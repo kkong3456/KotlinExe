b66b6b5f9d48df0f6b57f9b7d3b22da9f605401c
unknown <kkong3456@gmail.com>
2022-06-18 17:06:12 +0900

    first commit

---
+++ Kotlin/01/실습.kts
@@ -0,0 +1,12 @@
+var changeYes = 100
+//var change_yes //선언과 동시에 초기화 해야 함
+changeYes = 120
+
+println(changeYes)
+
+val CHANGE_NO = 100
+//CHANGE_NO=120  //val cannot be reassigned
+println(CHANGE_NO)
+
+val SCHOOL_NAME = "대한민국 학교"
+println(SCHOOL_NAME)
