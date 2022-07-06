be32e697276ab2903f07a95f6014086b48e66b49
unknown <kkong3456@gmail.com>
2022-07-07 07:36:02 +0900

    과제3완료

--- Kotlin/과제3.kts
+++ Kotlin/과제3.kts
@@ -1,61 +1,100 @@
 //계산기1
-class Calculator1(num1:Int,num2:Int){
-    val num1:Int
-    val num2:Int
-    init{
-        this.num1=num1
-        this.num2=num2
+class Calculator1 {
+    var result: Int = 0
+        set(value) {
+            field = value
+            println("중간 계산 값 : $field")
+        }
+
+    fun plusArth(inputNumber: Int) {
+        result += inputNumber
     }
 
-    fun plusArth():Int{
-        return num1+num2
+    fun minusArth(inputNumber: Int) {
+        result -= inputNumber
     }
-    fun minusArth():Int{
-        return num1-num2
+
+    fun productArth(inputNumber: Int) {
+        result *= inputNumber
     }
-    fun productArth():Int{
-        return num1*num2
-    }
-    fun divideArth():Int{
-        return num1/num2
+
+    fun divideArth(inputNumber: Int) {
+        result /= inputNumber
     }
 }
 
-val xxx1=Calculator1(5,5)
-println(xxx1.plusArth())
-println(xxx1.minusArth())
-println(xxx1.productArth())
-println(xxx1.divideArth())
+val xxx1 = Calculator1()
+xxx1.plusArth(5)
+xxx1.minusArth(3)
 
+
+//println(xxx1.minusArth())
+//println(xxx1.productArth())
+//println(xxx1.divideArth())
+
+println("1-----------------------------")
 //계산기2
-class Calculator2 constructor(num1:Int=0){
-    val num1:Int
-    init{
-        this.num1=num1
+class Calculator2 constructor(initialValue: Int = 0) {
+    var result: Int
+
+    init {
+        this.result = initialValue
     }
 
-    fun calculator(operator:Char,num2:Int):Int{
-        if(operator=='+'){
-            return num1+num2
-        }else if(operator=='-'){
-            return num1-num2
-        }else if(operator=='*'){
-            return num1*num2
-        }else if(operator=='/' && num2!=0){
-            return num1/num2
-        }else{
-            println("잘목된 연산입니다.")
-            return 0
+    fun calculator(operator: Char, num2: Int) {
+//        if (operator == '+') {
+//            return result += num2
+//        } else if (operator == '-') {
+//            return result -= num2
+//        } else if (operator == '*') {
+//            return result *= num2
+//        } else if (operator == '/' && num2 != 0) {
+//            return result /= num2
+//        } else {
+//            println("잘목된 연산입니다.")
+//            return 0
+//        }
+        when (operator) {
+            '+' -> this.result += num2
+            '-' -> this.result -= num2
+            '*' -> this.result *= num2
+            '/' -> this.result /= num2
+            else -> println("잘못된 연산입니다.")
         }
     }
 }
 
-val calculator=Calculator2(5)
-println(calculator.calculator('+',5))
-println(calculator.calculator('-',5))
-println(calculator.calculator('*',5))
-println(calculator.calculator('/',5))
-println(calculator.calculator('?',5))
+val calculator2 = Calculator2(10)
+calculator2.calculator('+', 10)
+println(calculator2.result)
 
+
+
+println("//22---------------------------------------------------")
 //계산기3
+//한번에 여러개의 연산을 입력받아야 함 vararg
+class Calculator3 {
+    var result: Int = 0
+
+    fun calculate(operators:List<Char>,inputNumbers:List<Int>){
+        operators.forEachIndexed { index, operator ->
+            _calculate(operator,inputNumbers[index])
+        }
+    }
+
+    fun _calculate(function: Char, inputNumber: Int) {
+        when (function) {
+            '+' -> this.result += inputNumber
+            '-' -> this.result -= inputNumber
+            '*' -> this.result *= inputNumber
+            '/' -> this.result /= inputNumber
+            else -> println("잘못된 연산입니다.")
+        }
+    }
+}
+
+val calculator3=Calculator3()
+calculator3.calculate(listOf<Char>('+','-'),listOf<Int>(10,20))
+println(calculator3.result)
+
 
