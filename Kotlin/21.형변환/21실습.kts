be1d6bed7bbac95af8ac300f4b72dddf0c2a2a63
kkong7 <kkongk@naver.com>
2022-07-11 16:08:14 +0900

    형변환 중

---
+++ Kotlin/21.형변환/21실습.kts
@@ -0,0 +1,12 @@
+//기본자료형의 타입캐스팅
+val number:Int=10
+var numberString:String=number.toString()
+//println("nuberString is $numberString")
+val stringToNumber:Int=numberString.toInt()
+println("stringToNumber is $stringToNumber")
+
+val numberF:Double=10.5
+var numberFToString:String=numberF.toString()
+println("numberFToString is $numberFToString")
+var numberFToInt:Int=numberF.toInt()
+println("numberFToInt is $numberFToInt")
